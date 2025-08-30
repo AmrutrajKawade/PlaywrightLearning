65dcf31168de9d51a864f503f1a3a4fcaea1f20f
Amrutraj Jeevan kawade <98204952+AmrutrajKawade@users.noreply.github.com>
2025-08-30 10:21:47 +0530

    Update operators2.js

--- tests/Javascript/operators2.js
+++ tests/Javascript/operators2.js
@@ -1,4 +1,4 @@
-let a = 10;
+/*let a = 10;
 let b = 7;
 
 console.log(a+b);
@@ -8,4 +8,18 @@ console.log(a/b);
 console.log(a%b);
 console.log(a**b);
 console.log(a==b);
-console.log(a===b);
+console.log(a===b);*/
+
+/*let k = 10;
+let m = k++ + ++k + ++k + k++
+ console.log(m);
+ console.log(k)*/
+
+
+let a = 10;
+let b = 5;
+
+a = a+b;
+b = a-b;
+a = a-b;
+
